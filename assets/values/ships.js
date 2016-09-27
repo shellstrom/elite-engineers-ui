5c0c697bb15b80f87025c027742ecafe6359ec06
SHELLSTROM <elite.engineers@shellstrom.se>
2016-09-27 16:29:27 +0200

    Calculator ship selection and reading

---
+++ assets/values/ships.js
@@ -0,0 +1,339 @@
+SHIPS =
+{
+    "ships": [{
+        "$schema": "http://cdn.coriolis.io/schemas/ship-loadout/3.json#",
+        "name": "corio Dependy plow",
+        "ship": "Asp Explorer",
+        "references": [
+            {
+                "name": "Coriolis.io",
+                "url": "https://coriolis.io/outfit/asp/0pjtjFjljdjsof5--1717------044e01--002h.Iw18WQ==.Aw18WQ==?bn=corio%20Dependy%20plow",
+                "code": "0pjtjFjljdjsof5--1717------044e01--002h.Iw18WQ==.Aw18WQ==",
+                "shipId": "asp"
+            }
+        ],
+        "components": {
+            "standard": {
+                "bulkheads": "Lightweight Alloy",
+                "cargoHatch": {
+                    "enabled": true,
+                    "priority": 1
+                },
+                "powerPlant": {
+                    "class": 5,
+                    "rating": "E",
+                    "enabled": true,
+                    "priority": 1
+                },
+                "thrusters": {
+                    "class": 5,
+                    "rating": "E",
+                    "enabled": true,
+                    "priority": 1
+                },
+                "frameShiftDrive": {
+                    "class": 5,
+                    "rating": "E",
+                    "enabled": true,
+                    "priority": 1
+                },
+                "lifeSupport": {
+                    "class": 4,
+                    "rating": "E",
+                    "enabled": true,
+                    "priority": 1
+                },
+                "powerDistributor": {
+                    "class": 4,
+                    "rating": "E",
+                    "enabled": true,
+                    "priority": 1
+                },
+                "sensors": {
+                    "class": 5,
+                    "rating": "E",
+                    "enabled": true,
+                    "priority": 1
+                },
+                "fuelTank": {
+                    "class": 5,
+                    "rating": "C",
+                    "enabled": true,
+                    "priority": 1
+                }
+            },
+            "hardpoints": [
+                null,
+                null,
+                {
+                    "class": 1,
+                    "rating": "F",
+                    "enabled": true,
+                    "priority": 1,
+                    "group": "Pulse Laser",
+                    "mount": "Fixed"
+                },
+                {
+                    "class": 1,
+                    "rating": "F",
+                    "enabled": true,
+                    "priority": 1,
+                    "group": "Pulse Laser",
+                    "mount": "Fixed"
+                },
+                null,
+                null
+            ],
+            "utility": [
+                null,
+                null,
+                null,
+                null
+            ],
+            "internal": [
+                {
+                    "class": 5,
+                    "rating": "E",
+                    "enabled": true,
+                    "priority": 1,
+                    "group": "Cargo Rack"
+                },
+                {
+                    "class": 5,
+                    "rating": "E",
+                    "enabled": true,
+                    "priority": 1,
+                    "group": "Shield Generator"
+                },
+                {
+                    "class": 2,
+                    "rating": "E",
+                    "enabled": true,
+                    "priority": 1,
+                    "group": "Cargo Rack"
+                },
+                null,
+                null,
+                {
+                    "class": 1,
+                    "rating": "E",
+                    "enabled": true,
+                    "priority": 1,
+                    "group": "Cargo Rack"
+                },
+                {
+                    "class": 1,
+                    "rating": "E",
+                    "enabled": true,
+                    "priority": 1,
+                    "group": "Scanner",
+                    "name": "Basic Discovery Scanner"
+                }
+            ]
+        },
+        "stats": {
+            "class": 2,
+            "hullCost": 6135660,
+            "speed": 250,
+            "boost": 340,
+            "boostEnergy": 14,
+            "agility": 6,
+            "baseShieldStrength": 140,
+            "baseArmour": 378,
+            "hullMass": 280,
+            "masslock": 11,
+            "pipSpeed": 0.13,
+            "moduleCostMultiplier": 1,
+            "fuelCapacity": 32,
+            "cargoCapacity": 38,
+            "ladenMass": 476,
+            "armourAdded": 0,
+            "armourMultiplier": 1,
+            "shieldMultiplier": 1,
+            "totalCost": 6661150,
+            "unladenMass": 406,
+            "totalDps": 6,
+            "armour": 378,
+            "powerAvailable": 13.6,
+            "powerRetracted": 7.84,
+            "powerDeployed": 8.62,
+            "unladenRange": 14.04,
+            "fullTankRange": 13.12,
+            "ladenRange": 12.07,
+            "unladenFastestRange": 133.9,
+            "ladenFastestRange": 122.86,
+            "maxJumpCount": 10,
+            "shieldStrength": 151.75,
+            "topSpeed": 253.77,
+            "topBoost": 345.13
+        }
+    },
+        {
+            "$schema": "http://cdn.coriolis.io/schemas/ship-loadout/3.json#",
+            "name": "minty pole",
+            "ship": "Federal Dropship",
+            "references": [
+                {
+                    "name": "Coriolis.io",
+                    "url": "https://coriolis.io/outfit/federal_dropship/0potoFjlodtsjf4-1717------04034e02--2h.Iw18aQ==.Aw18aQ==?bn=minty%20pole",
+                    "code": "0potoFjlodtsjf4-1717------04034e02--2h.Iw18aQ==.Aw18aQ==",
+                    "shipId": "federal_dropship"
+                }
+            ],
+            "components": {
+                "standard": {
+                    "bulkheads": "Lightweight Alloy",
+                    "cargoHatch": {
+                        "enabled": true,
+                        "priority": 1
+                    },
+                    "powerPlant": {
+                        "class": 6,
+                        "rating": "E",
+                        "enabled": true,
+                        "priority": 1
+                    },
+                    "thrusters": {
+                        "class": 6,
+                        "rating": "E",
+                        "enabled": true,
+                        "priority": 1
+                    },
+                    "frameShiftDrive": {
+                        "class": 5,
+                        "rating": "E",
+                        "enabled": true,
+                        "priority": 1
+                    },
+                    "lifeSupport": {
+                        "class": 5,
+                        "rating": "E",
+                        "enabled": true,
+                        "priority": 1
+                    },
+                    "powerDistributor": {
+                        "class": 6,
+                        "rating": "E",
+                        "enabled": true,
+                        "priority": 1
+                    },
+                    "sensors": {
+                        "class": 4,
+                        "rating": "E",
+                        "enabled": true,
+                        "priority": 1
+                    },
+                    "fuelTank": {
+                        "class": 4,
+                        "rating": "C",
+                        "enabled": true,
+                        "priority": 1
+                    }
+                },
+                "hardpoints": [
+                    null,
+                    {
+                        "class": 1,
+                        "rating": "F",
+                        "enabled": true,
+                        "priority": 1,
+                        "group": "Pulse Laser",
+                        "mount": "Fixed"
+                    },
+                    {
+                        "class": 1,
+                        "rating": "F",
+                        "enabled": true,
+                        "priority": 1,
+                        "group": "Pulse Laser",
+                        "mount": "Fixed"
+                    },
+                    null,
+                    null
+                ],
+                "utility": [
+                    null,
+                    null,
+                    null,
+                    null
+                ],
+                "internal": [
+                    {
+                        "class": 5,
+                        "rating": "E",
+                        "enabled": true,
+                        "priority": 1,
+                        "group": "Cargo Rack"
+                    },
+                    {
+                        "class": 4,
+                        "rating": "E",
+                        "enabled": true,
+                        "priority": 1,
+                        "group": "Cargo Rack"
+                    },
+                    {
+                        "class": 5,
+                        "rating": "E",
+                        "enabled": true,
+                        "priority": 1,
+                        "group": "Shield Generator"
+                    },
+                    {
+                        "class": 3,
+                        "rating": "E",
+                        "enabled": true,
+                        "priority": 1,
+                        "group": "Cargo Rack"
+                    },
+                    null,
+                    null,
+                    {
+                        "class": 1,
+                        "rating": "E",
+                        "enabled": true,
+                        "priority": 1,
+                        "group": "Scanner",
+                        "name": "Basic Discovery Scanner"
+                    }
+                ]
+            },
+            "stats": {
+                "class": 2,
+                "hullCost": 13469990,
+                "speed": 180,
+                "boost": 300,
+                "boostEnergy": 21,
+                "agility": 2,
+                "baseShieldStrength": 200,
+                "baseArmour": 540,
+                "hullMass": 580,
+                "masslock": 14,
+                "pipSpeed": 0.07,
+                "moduleCostMultiplier": 1,
+                "fuelCapacity": 16,
+                "cargoCapacity": 56,
+                "ladenMass": 848,
+                "armourAdded": 0,
+                "armourMultiplier": 1,
+                "shieldMultiplier": 1,
+                "totalCost": 14314210,
+                "unladenMass": 776,
+                "totalDps": 6,
+                "armour": 540,
+                "powerAvailable": 16.8,
+                "powerRetracted": 8.9,
+                "powerDeployed": 9.68,
+                "unladenRange": 7.37,
+                "fullTankRange": 7.25,
+                "ladenRange": 6.77,
+                "unladenFastestRange": 36.09,
+                "ladenFastestRange": 33.69,
+                "maxJumpCount": 5,
+                "shieldStrength": 103.91,
+                "topSpeed": 182.24,
+                "topBoost": 303.73
+            }
+        }
+    ]
+};
